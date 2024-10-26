62a1fc3151e8e548164a7b654f06bf8a5d6422c6
Mars-FeiFei <liuqingyuff@outlook.com>
2024-10-26 08:21:44 +0800

    Add Source Code Form.

---
+++ FeiSharpCodeEditor(WinForm.net8.0)/CodeCore/SourceCode.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace FeiSharpCodeEditor_WinForm.net8._0_.CodeCore
+{
+    partial class SourceCode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SourceCode));
+            richTextBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = Color.FromArgb(64, 64, 64);
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox1.ForeColor = Color.White;
+            richTextBox1.Location = new Point(0, 0);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(2383, 64);
+            richTextBox1.TabIndex = 0;
+            // 
+            // SourceCode
+            // 
+            AutoScaleDimensions = new SizeF(22F, 46F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2383, 1267);
+            Controls.Add(richTextBox1);
+            ForeColor = SystemColors.ControlText;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "SourceCode";
+            Text = "FeiSharpStudio-SourceCode";
+            Load += SourceCode_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox richTextBox1;
+    }
+}
